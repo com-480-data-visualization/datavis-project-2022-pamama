8b945bb84e0a54cd531ed8761d9cad111581023a
palomacito <paloma.cito@epfl.ch>
2022-05-18 12:43:53 +0200

    trying to access json files (it's hell)

--- website/js/script.js
+++ website/js/script.js
@@ -1,39 +1,50 @@
-const _play_pause_els = document.querySelectorAll('.section--body--item--play');
-/* Get all the 'Play' buttons */
-_play_pause_els.forEach(_el => {
-    /* When the user clicks the button */
-    _el.addEventListener('click', (e) => {
-        /* Get all the play buttons 
-            1. remove the 'active' class 
-            2. Change the 'pause' icon to the 'play' icon
-        */
-        _play_pause_els.forEach(_el2 => {
-            if(_el != _el2) {
-                _el2.classList.remove('active');
-                const _icon_el2 = _el2.querySelector('i');
+/*.getJSON("../data/top10artists.json", function(json) {
+        const header = document.querySelector('header');
+        const myH1 = document.createElement('h1');
+        myH1.textContent = obj['squadName'];
+        header.appendChild(myH1);
+      
+        const myPara = document.createElement('p');
+        myPara.textContent = `Hometown: ${obj['homeTown']} // Formed: ${obj['formed']}`;
+        header.appendChild(myPara);
+});
+   
+  
 
-                if(_icon_el2) {
-                    _icon_el2.classList.remove('lni-pause')
-                    _icon_el2.classList.add('lni-play');
-                }
-            }
-        });
+  const myObj = JSON.parse(myJSON);
+x = myObj["name"];
 
-        _el.classList.toggle('active');
-
-        const _icon_el = _el.querySelector('i');
-        if(_icon_el) {
-        /* Toggle the icon to 'pause' or 'play' */
-            if(_icon_el) {
-                if(_el.classList.contains('active')) {
-                    _icon_el.classList.remove('lni-play');
-                    _icon_el.classList.add('lni-pause');
-                } else {
-                    _icon_el.classList.remove('lni-pause')
-                    _icon_el.classList.add('lni-play');
-                }
-            }
+d3.json('../data/top10artists.json')
+    .then(data => {
+      data.forEach(artist => {
+        if(artist.Artist == 'Taylor Swift') {
+            var container = document.getElementById("artist1");
+            var div = document.createElement("div");
+            div.innerHTML = artist.Artist;
+            container.appendChild(div);
         }
     });
-});
 
+    });*/
+
+    fetch('../../data/top10artists.json')
+    .then(function (response) {
+        console.log('ok')
+        return response.json();
+    })
+    .then(function (data) {
+        appendData(data);
+    })
+    .catch(function (err) {
+        console.log('error: ' + err);
+    });
+
+    function appendData(data) {
+        var mainContainer = document.getElementById("myData");
+        for (var i = 0; i < data.length; i++) {
+            var div = document.createElement("div");
+            div.innerHTML = 'Rank: ' + data[i].Rank + ' ' + data[i].Artist;
+            mainContainer.appendChild(div);
+            console.log()
+        }
+    }
